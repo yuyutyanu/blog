9a7cb0bf294cd8598ffaeb312132a082a6c70a4b
chanu <yuyutyanu@yahoo.co.jp>
2018-02-04 22:04:20 +0900

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,10 +15,14 @@ class DatabaseSeeder extends Seeder
         DB::table('Blog')->insert([
             'title' => 'ほげほげ',
             'article' => 'ほげほげ',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
         ]);
         DB::table('Blog')->insert([
             'title' => '誰でも書けるよ',
             'article' => 'ほげほげ',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
         ]);
     }
 }
